38d1708dbe1e66884f490a408783878ef85adbec
saravanan.thangavel <saravanan.thangavel@cloudsolutions.com.sa>
2022-07-18 14:10:44 +0300

    initial commit

---
+++ DMS.Data/Migrations/20220718095315_yourMigrationName.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class yourMigrationName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UsersUserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Documents_Users_UsersUserId",
+                table: "Documents");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UsersUserId",
+                table: "Categories",
+                column: "UsersUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Documents_Users_UsersUserId",
+                table: "Documents",
+                column: "UsersUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UsersUserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Documents_Users_UsersUserId",
+                table: "Documents");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UsersUserId",
+                table: "Categories",
+                column: "UsersUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Documents_Users_UsersUserId",
+                table: "Documents",
+                column: "UsersUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
